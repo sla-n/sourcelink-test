8ed44bed7f7fc1e9618ee9d7ef9512ef6b9fe8e0
slawomir.nidecki <slawomir.nidecki@PLPNB07183>
2021-08-07 06:03:30 +0200

    Add project files.

---
+++ sourcelink-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sourcelink_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
